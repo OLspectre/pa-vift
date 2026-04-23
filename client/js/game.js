068599057464ef6556c204973942522ec227d0a1
OLspectre <ossi9910@gmail.com>
2026-04-23 12:59:07 +0200

    mapBtn now changes to map page.

--- client/js/game.js
+++ client/js/game.js
@@ -5,10 +5,9 @@ const endClueBtn = document.querySelector("#endClueBtn");
 const partCardContainner = document.querySelector("#destinationCardContainer");
 const endCardContainer = document.querySelector("#endCardContainer");
 
-buttonContainer.addEventListener("click", function(e) {
+const mapBtn = document.querySelector("#mapBtn");
 
-    
-    
+buttonContainer.addEventListener("click", function (e) {
     if (!e.target.classList.contains("selected")) {
         endClueBtn.classList.toggle("selected");
         partClueBtn.classList.toggle("selected");
@@ -17,14 +16,19 @@ buttonContainer.addEventListener("click", function(e) {
             partCardContainner.style.display = "none"
             endCardContainer.style.display = "flex"
         }
-        
+
         if (e.target != endClueBtn) {
             endCardContainer.style.display = "none"
             partCardContainner.style.display = "flex"
         }
 
     }
-    
-    
 })
+
+
+mapBtn.addEventListener("click", () => {
+    window.location.href = "../pages/questMap.html";
+})
+
+
 
