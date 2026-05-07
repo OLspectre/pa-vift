54c9c12e1367def9a0e9f3ddba9b178ad7f41782
swesslen <simon@jolowe.se>
2026-05-07 15:21:04 +0200

    gameEnd js leaderboard sumin sumin

--- client/js/gameEnd.js
+++ client/js/gameEnd.js
@@ -1,2 +1,22 @@
+import { teams } from "../../data/teams.js"
+
 const team = JSON.parse(localStorage.getItem("team"));
 
+const leaderboard = document.querySelector("#leaderboard");
+
+function createLeaderboard() {
+for (team in teams) {
+    const teamNameDiv = document.createElement("div");
+    const teamTimeDiv = document.createElement("div");
+    const teamPointsDiv = document.createElement("div");
+
+    leaderboard.appendChild(teamNameDiv);
+    leaderboard.appendChild(teamTimeDiv);
+    leaderboard.appendChild(teamPointsDiv);
+
+    teamNameDiv.innerHTML = `${team.name}`;
+    teamTimeDiv.innerHTML = `/* ??? */`;
+    teamPointsDiv.innerHTML = `/* ??? */`;
+
+}
+}
