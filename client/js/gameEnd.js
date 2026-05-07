0a6d5a10a1ef3e096045e61c9d993cc324931761
swesslen <simon@jolowe.se>
2026-05-07 15:34:11 +0200

    leaderboard with content

--- client/js/gameEnd.js
+++ client/js/gameEnd.js
@@ -1,14 +1,30 @@
 import { teams } from "../../data/teams.js"
 
-const team = JSON.parse(localStorage.getItem("team"));
+// const team = JSON.parse(localStorage.getItem("team"));
 
 const leaderboard = document.querySelector("#leaderboard");
 
-function createLeaderboard() {
-for (team in teams) {
+(function createLeaderboard() {
+for (let team of teams) {
     const teamNameDiv = document.createElement("div");
     const teamTimeDiv = document.createElement("div");
     const teamPointsDiv = document.createElement("div");
+
+    teamNameDiv.style.border = "1px solid var(--hColor)";
+    teamTimeDiv.style.border = "1px solid var(--hColor)";
+    teamPointsDiv.style.border = "1px solid var(--hColor)";
+
+    teamNameDiv.style.height = "42px";
+    teamTimeDiv.style.height = "42px";
+    teamPointsDiv.style.height = "42px";
+
+    teamNameDiv.style.padding = "4px 8px";
+    teamTimeDiv.style.padding = "4px 8px";
+    teamPointsDiv.style.padding = "4px 8px";
+
+    teamNameDiv.style.alignContent = "center";
+    teamTimeDiv.style.alignContent = "center";
+    teamPointsDiv.style.alignContent = "center";
 
     leaderboard.appendChild(teamNameDiv);
     leaderboard.appendChild(teamTimeDiv);
@@ -17,6 +33,5 @@ for (team in teams) {
     teamNameDiv.innerHTML = `${team.name}`;
     teamTimeDiv.innerHTML = `/* ??? */`;
     teamPointsDiv.innerHTML = `/* ??? */`;
-
 }
-}
+})();
