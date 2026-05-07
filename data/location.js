a44b4048c4e53257ab3983f699708335c28585c4
swesslen <simon@jolowe.se>
2026-05-07 10:35:51 +0200

    location challs

--- data/location.js
+++ data/location.js
@@ -7,7 +7,7 @@ export const locationsData = [
         locationID: 1,
         hint: "Den gyllene måsen är nära till hands och tittar ut över vattnet",
         acceptableAnswers: ["McDonalds anna lindh plats", "anna lindh plats McDonalds", "McDonalds anna lindhs plats", "anna lindhs plats McDonalds", "McDonalds anna linds plats", "anna linds plats McDonalds"],
-        challenge: "Meny beställning: Nr på menyn ska skrivas in som svar. Ex: min beställning: big mac (nr 2), mcflurry (nr 12), nuggets (nr 8). svar: 2128",
+        challenge: "Här är min beställning: Big Mac mål, 4 bitar chicken nuggets och en sundae utan topping.",
         challAnswer: "x",
         mainClueHint: "Vi lämnar fast mark och närmar oss något vars namn skulle kunna beskriva en helt vanlig bostad, men där alla har känt sig långt ifrån hemma."
     },
@@ -15,8 +15,8 @@ export const locationsData = [
         locationID: 2,
         hint: "När solen går ner vaknar huset till liv. Fjärdedelar av ljud. Här stiger ljudet våning för våning. Från golv till golv förändras genren.",
         acceptableAnswers: ["etage", "étage"],
-        challenge: "",
-        challAnswer: "x",
+        challenge: "En lapp sitter uppsatt utanför Etage, leta upp den…",
+        challAnswer: "Alla vill till himlen men ingen vill dö",
         mainClueHint: "Jag står där historien sitter i vägg, men jag är inte levande – ändå full av skägg. Här kan du resa i tid utan att gå, och möta både nutid och det som var då.",
 
     },
@@ -24,22 +24,22 @@ export const locationsData = [
         locationID: 3,
         hint: "Här lånas tankar ut utan att försvinna, och historier lämnar spår utan att gå vilse.",
         acceptableAnswers: ["stadsbiblioteket", "malmö stadsbibliotek"],
-        challenge: "platsspecifikt för bibblan",
-        challAnswer: "x",
+        challenge: "Ta er till ljusets kalender och leta upp nr 746",
+        challAnswer: "Konst & Foto",
         mainClueHint: "Där språk och herrar byttes utan att jag flyttades. Jag var en dansk försvarsvakt, men blev svensk historia."
     },
     {
         locationID: 4,
         hint: "Jag är en krona som blivit till betong, och en prins som aldrig regerade.",
         acceptableAnswers: ["kronprinsen", "kronprinsen köpcentrum"],
-        challenge: "platsspecifikt för kronprinsen",
-        challAnswer: "x",
+        challenge: "Hitta skärmen där man kan få all information. Här letar vi efter nr 4…",
+        challAnswer: "Hantverket",
         mainClueHint: "Jag är det mest kända slottet i Malmö, omringad av vallgravar visar jag upp min och stadens historia."
     }, {
         locationID: 5,
         hint: "Jag är där asfalt ger plats åt löv och tystare steg, där bänkar tar över efter trafikens brus. Jag ligger där stadens sista steg blir utsikt, och där mitt namn låter som havet intill.",
         acceptableAnswers: ["öresundspark", "öresundsparken"],
-        challAnswer: "x",
-        challenge: "Skulpturen Vinden - man ska leta upp statyn och skriva in år."
+        challAnswer: "1959",
+        challenge: "Leta efter skulpturen Vinden."
     }
 ];
